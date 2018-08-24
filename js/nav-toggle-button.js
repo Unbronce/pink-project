72edcfb49710db67a8dda39357cdb17b6faffce6
Doroshenko Yaroslav <ezah02021996@gmail.com>
2018-08-24 18:19:43 +0300

    mobile and tablet header have done

---
+++ js/nav-toggle-button.js
@@ -0,0 +1,36 @@
+var navMain = document.querySelector('.main-nav');
+  var navToggle = document.querySelector('.main-nav__toggle');
+  var navHeader = document.querySelector('.page-header')
+
+  navMain.classList.remove('main-nav--nojs');
+
+  navToggle.addEventListener('click', function() {
+    if (navMain.classList.contains('main-nav--closed')) {      
+      navMain.classList.remove('main-nav--closed');
+      navHeader.classList.remove('page-header--transparent');      
+      navMain.classList.add('main-nav--opened');
+    } else {
+      navMain.classList.add('main-nav--closed');
+      navHeader.classList.add('page-header--transparent');
+      navMain.classList.remove('main-nav--opened');
+      
+    }
+  });
+
+// var navMain = document.querySelector('.main-nav');
+//   var navToggle = document.querySelector('.main-nav__toggle');
+
+
+//   navMain.classList.remove('main-nav--nojs');
+
+//   navToggle.addEventListener('click', function() {
+//     if (navMain.classList.contains('main-nav--closed')) {      
+//       navMain.classList.remove('main-nav--closed');  
+//       navMain.classList.add('main-nav--opened');
+//     } else {
+//       navMain.classList.add('main-nav--closed');
+//       navMain.classList.remove('main-nav--opened');
+      
+//     }
+//   });
+
