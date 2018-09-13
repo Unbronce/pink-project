ef321f3a392e7afa3ba84e4f4f0df3019e5fb4e2
Doroshenko Yaroslav <ezah02021996@gmail.com>
2018-09-13 09:02:56 +0300

    have returned to the previous version

--- js/nav-toggle-button.js
+++ js/nav-toggle-button.js
@@ -1,28 +1,8 @@
-// var navMain = document.querySelector('.main-nav');
-//   var navToggle = document.querySelector('.main-nav__toggle');
-//   var navHeader = document.querySelector('.page-header')
-
-//   navMain.classList.remove('main-nav--nojs');
-
-//   navToggle.addEventListener('click', function() {
-//     if (navMain.classList.contains('main-nav--closed')) {      
-//       navMain.classList.remove('main-nav--closed');
-//       navHeader.classList.remove('page-header--transparent');      
-//       navMain.classList.add('main-nav--opened');
-//     } else {
-//       navMain.classList.add('main-nav--closed');
-//       navHeader.classList.add('page-header--transparent');
-//       navMain.classList.remove('main-nav--opened');      
-//     }
-//   });
-
-
 var navMain = document.querySelector('.main-nav');
   var navToggle = document.querySelector('.main-nav__toggle');
   var navHeader = document.querySelector('.page-header')
 
   navMain.classList.remove('main-nav--nojs');
-  navHeader.classList.add('page-header--transparent');  
 
   navToggle.addEventListener('click', function() {
     if (navMain.classList.contains('main-nav--closed')) {      
@@ -35,3 +15,23 @@ var navMain = document.querySelector('.main-nav');
       navMain.classList.remove('main-nav--opened');      
     }
   });
+
+
+// var navMain = document.querySelector('.main-nav');
+//   var navToggle = document.querySelector('.main-nav__toggle');
+//   var navHeader = document.querySelector('.page-header')
+
+//   navMain.classList.remove('main-nav--nojs');
+//   navHeader.classList.add('page-header--transparent');  
+
+//   navToggle.addEventListener('click', function() {
+//     if (navMain.classList.contains('main-nav--closed')) {      
+//       navMain.classList.remove('main-nav--closed');
+//       navHeader.classList.remove('page-header--transparent');      
+//       navMain.classList.add('main-nav--opened');
+//     } else {
+//       navMain.classList.add('main-nav--closed');
+//       navHeader.classList.add('page-header--transparent');
+//       navMain.classList.remove('main-nav--opened');      
+//     }
+//   });
