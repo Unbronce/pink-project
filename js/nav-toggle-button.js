1bff7bc6883623cd79a1006d3bde07940a6132af
Doroshenko Yaroslav <ezah02021996@gmail.com>
2018-09-13 10:00:58 +0300

    fixed nav-list, works with/without js

--- js/nav-toggle-button.js
+++ js/nav-toggle-button.js
@@ -3,6 +3,11 @@ var navMain = document.querySelector('.main-nav');
   var navHeader = document.querySelector('.page-header')
 
   navMain.classList.remove('main-nav--nojs');
+  navHeader.classList.add('page-header--transparent'); 
+  navMain.classList.remove('main-nav--opened');
+  navMain.classList.add('main-nav--closed');
+  
+
 
   navToggle.addEventListener('click', function() {
     if (navMain.classList.contains('main-nav--closed')) {      
@@ -23,6 +28,34 @@ var navMain = document.querySelector('.main-nav');
 
 //   navMain.classList.remove('main-nav--nojs');
 //   navHeader.classList.add('page-header--transparent');  
+
+//   navToggle.addEventListener('click', function() {
+//     if (navMain.classList.contains('main-nav--closed')) {      
+//       navMain.classList.remove('main-nav--closed');
+//       navHeader.classList.remove('page-header--transparent');      
+//       navMain.classList.add('main-nav--opened');
+//     } else {
+//       navMain.classList.add('main-nav--closed');
+//       navHeader.classList.add('page-header--transparent');
+//       navMain.classList.remove('main-nav--opened');      
+//     }
+//   });
+
+
+
+
+
+
+
+
+// var navMain = document.querySelector('.main-nav');
+// var navToggle = document.querySelector('.main-nav__toggle');
+// var navHeader = document.querySelector('.page-header')
+
+// navMain.classList.remove('main-nav--nojs');
+// navHeader.classList.add('page-header--transparent');  
+// navHeader.classList.add('main-nav--opened');
+
 
 //   navToggle.addEventListener('click', function() {
 //     if (navMain.classList.contains('main-nav--closed')) {      
