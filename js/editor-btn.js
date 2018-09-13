0de6fe714a65f2004e1f1c5b78eef0df6bf2fee0
Doroshenko Yaroslav <ezah02021996@gmail.com>
2018-09-13 08:47:59 +0300

    have fixed problem with background in page-header when js is absent

---
+++ js/editor-btn.js
@@ -0,0 +1,19 @@
+var crop = document.getElementById("crop");
+var fill = document.getElementById("fill");
+var contrast = document.getElementById("contrast");
+
+crop.addEventListener("click", function (evt) {
+  evt.preventDefault();
+});
+
+fill.addEventListener("click", function (evt) {
+  evt.preventDefault();
+ 
+});
+
+contrast.addEventListener("click", function (evt) {
+  evt.preventDefault();
+});
+
+
+// evt.preventDefault();
--- js/nav-toggle-button.js
+++ js/nav-toggle-button.js
@@ -1,8 +1,28 @@
+// var navMain = document.querySelector('.main-nav');
+//   var navToggle = document.querySelector('.main-nav__toggle');
+//   var navHeader = document.querySelector('.page-header')
+
+//   navMain.classList.remove('main-nav--nojs');
+
+//   navToggle.addEventListener('click', function() {
+//     if (navMain.classList.contains('main-nav--closed')) {      
+//       navMain.classList.remove('main-nav--closed');
+//       navHeader.classList.remove('page-header--transparent');      
+//       navMain.classList.add('main-nav--opened');
+//     } else {
+//       navMain.classList.add('main-nav--closed');
+//       navHeader.classList.add('page-header--transparent');
+//       navMain.classList.remove('main-nav--opened');      
+//     }
+//   });
+
+
 var navMain = document.querySelector('.main-nav');
   var navToggle = document.querySelector('.main-nav__toggle');
   var navHeader = document.querySelector('.page-header')
 
   navMain.classList.remove('main-nav--nojs');
+  navHeader.classList.add('page-header--transparent');  
 
   navToggle.addEventListener('click', function() {
     if (navMain.classList.contains('main-nav--closed')) {      
@@ -12,25 +32,6 @@ var navMain = document.querySelector('.main-nav');
     } else {
       navMain.classList.add('main-nav--closed');
       navHeader.classList.add('page-header--transparent');
-      navMain.classList.remove('main-nav--opened');
-      
+      navMain.classList.remove('main-nav--opened');      
     }
   });
-
-// var navMain = document.querySelector('.main-nav');
-//   var navToggle = document.querySelector('.main-nav__toggle');
-
-
-//   navMain.classList.remove('main-nav--nojs');
-
-//   navToggle.addEventListener('click', function() {
-//     if (navMain.classList.contains('main-nav--closed')) {      
-//       navMain.classList.remove('main-nav--closed');  
-//       navMain.classList.add('main-nav--opened');
-//     } else {
-//       navMain.classList.add('main-nav--closed');
-//       navMain.classList.remove('main-nav--opened');
-      
-//     }
-//   });
-
